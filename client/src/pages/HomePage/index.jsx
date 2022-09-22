960fce053d502c13da562d8ba464383dc2acc759
CoGuisMod <coguismod@gmail.com>
2022-09-22 13:50:46 -0500

    Home page completed.

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { UserAuth } from "../../context/AuthContext";
+/* Style import */
+import "./style.css";
+
+/* Images imports */
+import HeroImg from "../../assets/hero.png";
 
 const index = () => {
-  const { user } = UserAuth();
-
-  console.log(user);
-
   return (
-    <section className=" pt-16 h-screen">
-      <Link to="/login">LogIn</Link>
+    <section className="max-w-7xl w-full h-screen mx-auto pt-24 px-8">
+      <div className="flex">
+        <div className="grow max-w-xl pr-8">
+          <h1 className="font-medium text-4xl mb-4">
+            The application that helps you organize and plan{" "}
+            <Link to="/" className="brand">
+              <span className="brand-one">What</span>
+              <span className="brand-two">To</span>Do
+              <span className="brand-three">?</span>
+            </Link>
+          </h1>
+          <p className="mb-8">
+            Make books, create lists and add thing to do! Orgnize your schedule,
+            goals and tasks as easy as that.
+          </p>
+          <Link to="/signup" className="home_button">
+            Register now!
+          </Link>
+        </div>
+        <div className="grow">
+          <img
+            src={HeroImg}
+            alt="ToDo Skeleton Website"
+            className="rounded-3xl w-full overflow-hidden home_hero-shadow"
+          />
+        </div>
+      </div>
     </section>
   );
 };
